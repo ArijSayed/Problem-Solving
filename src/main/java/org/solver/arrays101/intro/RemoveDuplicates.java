25e0eaea9612bb8d9b0398d5efcc233e5dab3bd1
ArijSayed <ariij.sayed@gmail.com>
2023-06-10 22:03:13 +0300

    Added another solution for RemoveDuplicates problem

--- src/main/java/org/solver/arrays101/intro/RemoveDuplicates.java
+++ src/main/java/org/solver/arrays101/intro/RemoveDuplicates.java
@@ -5,10 +5,10 @@ import java.util.Arrays;
 public class RemoveDuplicates {
     public static void test(){
         int [] arr={1,1,2};
-//        System.out.println("The input is: "+Arrays.toString(arr));
+        System.out.println("The input is: "+Arrays.toString(arr));
         removeDuplicates(arr);
-//        System.out.println(removeDuplicates(arr));
-//        System.out.println("The output is: "+Arrays.toString(arr));
+        System.out.println(removeDuplicates(arr));
+        System.out.println("The output is: "+Arrays.toString(arr));
     }
     public static int removeDuplicates(int[] arr) {
 
@@ -33,20 +33,38 @@ public class RemoveDuplicates {
 //        return j;
 
 
-        //another solution
-        int j = 0;
-        if (arr.length == 0 || arr.length == 1) {
-            j = arr.length;
-            return j;
+        //another solution time complexity O(N) space complexity O(1)
+//        int j = 0;
+//        if (arr.length == 0 || arr.length == 1) {
+//            j = arr.length;
+//            return j;
+//        }
+//        for (int i = 0; i < arr.length - 1; i++) {
+//            if (arr[i] != arr[i + 1]) {
+//                arr[j] = arr[i];
+//                j++;
+//            }
+//        }
+//        arr[j] = arr[arr.length - 1];
+//        j++;
+//        return j;
+
+        //another solution using two pointers reader and writer
+        if (arr == null) {
+            return 0;
         }
-        for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i] != arr[i + 1]) {
-                arr[j] = arr[i];
-                j++;
+        // The first element shouldn't be touched; it's already in its correct place.
+        int writePointer = 1;
+        for (int readPointer = 1; readPointer < arr.length; readPointer++) {
+            if (arr[readPointer] != arr[readPointer - 1]) {
+                // Copy it into the next position at the front, tracked by writePointer.
+                arr[writePointer] = arr[readPointer];
+                // And we need to now increment writePointer, because the next element
+                // should be written one space over.
+                 writePointer++;
             }
         }
-        arr[j] = arr[arr.length - 1];
-        j++;
-        return j;
+        // This turns out to be the correct length value.
+        return writePointer;
     }
 }
